1afa77a905767ff54298bd83fb260f5b9293525a
LaBlanca Ponder <lablancaponder@gmail.com>
2017-12-08 17:22:02 -0700

    Add variable for words to choose from

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+// Different words to choose from
+var words =["bootcamp", "denver", "coding", "javascript", "html", "boostrap", "github", "jquery", "loops", "array", "console log", "jay", "trilogy", "web development", "front end", "back end", "full stack"];
+
